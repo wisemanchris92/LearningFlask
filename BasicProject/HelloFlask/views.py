e76d0ce47acc56e95752812f3a6def9a376193f4
ChrisWiseman <wisemanchris92@gmail.com>
2020-03-31 19:33:32 -0400

    added about page

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import render_template
 from HelloFlask import app
 
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -13,3 +14,16 @@ def home():
         title = "Hello Flask",
         message = "Hello, Flask!",
         content = " on " + formatted_now)
+
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title = "About HelloFlask",
+        content = "Example app page for Flask.")
+
+
+@app.route('/api/data')
+def get_data():
+  return app.send_static_file('data.json')
